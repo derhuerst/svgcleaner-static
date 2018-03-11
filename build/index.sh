6ea5d78646e8abba23f283a9e81287ac24f0ddf7
Jannis R <mail@jannisr.de>
2018-03-11 16:07:47 +0100

    Windows CI build 💚
    commit 854c114ebcf880912b080b49aadbfde8c85ba658
    Author: Jannis R <mail@jannisr.de>
    Date:   Sun Mar 11 16:04:39 2018 +0100
    
        apparently os.arch() is broken :bug:
    
    commit 498c921e18c6f9471b916c284a4c59e59afac7c0
    Author: Jannis R <mail@jannisr.de>
    Date:   Sun Mar 11 15:29:07 2018 +0100
    
        trigger another Appveyor build 💚
    
    commit 5526142a959b8c379f753040279ddcc37e0854b2
    Author: Jannis R <mail@jannisr.de>
    Date:   Sun Mar 11 15:21:59 2018 +0100
    
        doh! 💚
    
    commit d8114ace2af8eee78e0a0cfa2f0270c3f5b67ebf
    Author: Jannis R <mail@jannisr.de>
    Date:   Sun Mar 11 15:20:03 2018 +0100
    
        try to fix Windows 💚
    
    commit 996b7103c5cac01aff80cd2ea43d07312ddeeca2
    Author: Jannis R <mail@jannisr.de>
    Date:   Sun Mar 11 15:16:15 2018 +0100
    
        Appveyor Windows build

--- build/index.sh
+++ build/index.sh
@@ -12,18 +12,18 @@ if [ $? -ne 0 ]; then
 fi
 
 echo 'downloading linux asset'
-linuxUrl=$(./asset-url.js linux)
+linuxUrl=$(node asset-url.js linux)
 download $linuxUrl linux.tar.gz
 $tar -xz -C ../bin/linux/x64 -f linux.tar.gz svgcleaner
 
 echo 'downloading macOS asset'
-macOSUrl=$(./asset-url.js macOS)
+macOSUrl=$(node asset-url.js macOS)
 download $macOSUrl macOS.zip
 unzip -o -d . -j macOS.zip svgcleaner
 mv svgcleaner ../bin/darwin/x64/svgcleaner
 
 echo 'downloading Windows asset'
-windowsUrl=$(./asset-url.js windows)
+windowsUrl=$(node asset-url.js windows)
 download $windowsUrl windows.zip
 unzip -o -d . -j windows.zip svgcleaner-cli.exe
 mv svgcleaner-cli.exe ../bin/win32/x64/svgcleaner.exe
